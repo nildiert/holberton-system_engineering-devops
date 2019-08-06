cdd026cf979a507406ff9af842bc79559263b9a5
nildiert <niljordan23@gmail.com>
2019-08-06 16:35:51 +0000

    [FIX] Fixed the style of puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a puppet file
 file { 'holberton':
-    path => '/tmp/holberton',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet'
 }
