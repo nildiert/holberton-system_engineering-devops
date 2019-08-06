c18e5ae572a22a0dcbf5f62b2c3087c3f60bcd14
nildiert <niljordan23@gmail.com>
2019-08-06 16:08:36 +0000

    [FIX] Fixed style again ...

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-B1;5201;0c#!/usr/bin/env bash
-#Create a puppet file
+#!/usr/bin/env bash
+# Create a puppet file
 file { '/tmp/holberton':
-mode => '0744',
-owner => www-data,
-group => www-data,
-content => 'I love Puppet'
+    mode => '0744',
+    owner => www-data,
+    group => www-data,
+    content => 'I love Puppet'
 }
