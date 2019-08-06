c87c9e00cb5cd214e4466fa1c812c3b85377bda3
nildiert <niljordan23@gmail.com>
2019-08-06 16:03:59 +0000

    [FEATURE] Fixed shellcheck style in file 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 #Create a puppet file
-
-file { "/tmp/holberton":
+file { '/tmp/holberton':
   mode => '0744',
   owner => www-data,
   group => www-data,
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
