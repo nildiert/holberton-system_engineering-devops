6b8c8facbe73c0b78928572d7e9f0da8183f5f97
nildiert <niljordan23@gmail.com>
2019-08-19 16:11:25 +0000

    [FEATURE] Added file 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,25 @@
 #install nginx using puppet
 package{ "nginx":
-  ensure => '1.10.3',
+ensure => 'present',
+}
+-> file { '/tmp/holberton':
+ensure    => 'present',
+  path    => '/var/www/index.html',
+  content => 'Holberton School',
+}
+-> file { '/etc/nginx/sites-enabled/default':
+ensure  => 'present',
+}
+-> file_line { 'Adding redirection':
+  path  => '/etc/nginx/sites-enabled/default',
+  line  => 'server {
+        location /redirect_me {
+                 return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }',
+  match => '^server {',
+}
+
+-> exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => 'shell'
 }
