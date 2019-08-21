a3aad86a57a6bd197d280b97f6baf6d542df9f2d
nildiert <niljordan23@gmail.com>
2019-08-21 02:44:35 +0000

    [FIX] Fixed file 3 and fle 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,11 +7,11 @@ ensure    => 'present',
   path    => '/var/www/index.html',
   content => 'Holberton School',
 }
--> file { '/etc/nginx/sites-enabled/default':
+-> file { '/etc/nginx/sites-available/default':
 ensure  => 'present',
 }
 -> file_line { 'Adding redirection':
-  path  => '/etc/nginx/sites-enabled/default',
+  path  => '/etc/nginx/sites-available/default',
   line  => 'server {
         location /redirect_me {
                  return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
@@ -22,4 +22,9 @@ ensure  => 'present',
 -> exec { 'restart':
   command  => 'sudo service nginx restart',
   provider => 'shell'
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
