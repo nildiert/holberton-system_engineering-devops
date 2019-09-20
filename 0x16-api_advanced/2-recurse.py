51ac6190641eb45ae2765fb26c3541f59cd00c09
nildiert <niljordan23@gmail.com>
2019-09-20 04:51:35 +0000

    [FIX] Fixed file 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 ''' Recursive function '''
 import requests
-from sys import argv
 
 
 def recurse(subreddit, hot_list=[], after=""):
     ''' Recursive function '''
-    try:
-        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
-            argv[1], after)
-        user_agent = "Nildiert"
-        headers = {'User-Agent': user_agent}
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response:
-            data = response.json().get('data')
-            children = data.get('children')
-            after = data.get('after')
-            [hot_list.append(tit.get('data').get('title') for ti in children)]
-            if after is not None and children:
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+        subreddit, after)
+    user_agent = "Nildiert"
+    headers = {'User-Agent': user_agent}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response:
+        data = response.json().get('data')
+        children = data.get('children')
+        after = data.get('after')
+        [hot_list.append(tit.get('data').get('title') for ti in children)]
+        if after is not None and children:
+            return recurse(subreddit, hot_list, after)
         else:
-            return None
-    except:
+            return hot_list
+    else:
         return None
