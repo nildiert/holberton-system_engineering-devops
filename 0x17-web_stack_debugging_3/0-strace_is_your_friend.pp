204e80dc674166ea29fbc6ec4eac11db14a04c89
nildiert <niljordan23@gmail.com>
2019-10-02 23:40:30 +0000

    [FEATURE] Added file 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Sed command
 file_line { 'Exec file':
   path  => '/var/www/html/wp-settings.php',
-  line  => 'php',
+  line  => 'require_once( ABSPATH . WPINC . "/class-wp-locale.php" );',
   match => 'phpp'
 }
