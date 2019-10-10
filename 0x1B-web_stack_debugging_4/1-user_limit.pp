00dc5442c72beda9433f1909a0d7637dafd6a1a6
nildiert <niljordan23@gmail.com>
2019-10-10 22:57:22 +0000

    [FEATURE] Added file 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Change line
+exec { '/etc/default/nginx':
+  command  => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1000/' /etc/security/limits.conf",
+  provider => 'shell'
+}
+-> exec { '/etc/security/limits.conf':
+  command  => "sed -i 's/holberton hard nofile 5/holberton hard nofile 1000/' /etc/security/limits.conf",
+  provider => 'shell'
+}
+-> exec { 'Restart command':
+  command  => "sysctl -p",
+  provider => 'shell'
+}
